a003527fddb26e0ab7d1ec561308582ad53072ab
ria0864 <dbwls08640@gmail.com>
2021-10-06 19:35:41 +0900

    feat(view): 입출력 기능

---
+++ src/main/java/baseball/consts/Message.java
@@ -0,0 +1,18 @@
+package baseball.consts;
+
+public enum Message {
+ READ_USER_NUMBER("숫자를 입력해주세요 : "),
+ CLEAR_GAME("3개의 숫자를 모두 맞히셨습니다! 게임 끝"),
+ RESTART_OR_END_GAME("게임을 새로 시작하려면 1을 입력하세요. 1을 제외한 문자 입력 시, 게임은 종료됩니다."),
+ EXIT_GAME("게임을 완전히 종료합니다.");
+
+ private String message;
+
+ Message(String message) {
+  this.message = message;
+ }
+
+ public String getMessage() {
+  return message;
+ }
+}
---
+++ src/main/java/baseball/view/InputConsole.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+import nextstep.utils.Console;
+
+public class InputConsole {
+
+ public String readUserInput() {
+  return Console.readLine();
+ }
+}
