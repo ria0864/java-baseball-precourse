e7e1ca19f2f4b8ac4f5acc3d6abefaa944a6d69e
ria0864 <dbwls08640@gmail.com>
2021-10-06 23:25:36 +0900

    feat(Player): 입력한 숫자에 대한 게임 로직, 게임 결과 합산

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -6,10 +6,73 @@ import baseball.constants.ErrorMessage;
 import static baseball.constants.BaseballConstants.*;
 
 public class Player {
+ private final ArrayList<Integer> numbers = new ArrayList<>();
+ private int strike;
+ private int ball;
  private final Pattern regExp = Pattern.compile("^[1-9]*$");
 
  public Player() {
 
+ }
+
+ public void initPlayer() {
+  numbers.clear();
+  strike = 0;
+  ball = 0;
+ }
+
+ public String play(String userInput, List<Integer> computerNumbers) {
+  String validationResult = CheckValidationInputData(userInput);
+  if (validationResult != null) {
+   return validationResult;
+  }
+
+  addNumbers(userInput);
+
+  for (int index = 0; index < NUMBER_SIZE; index++) {
+   int number = computerNumbers.get(index);
+   countStrike(index, number);
+   countBall(index, number);
+  }
+  return mergeResult();
+ }
+
+ private void countStrike(int index, int number) {
+  if (numbers.get(index).equals(number)) {
+   strike++;
+  }
+ }
+
+ private void countBall(int index, int number) {
+  if (numbers.contains(number)) {
+   if (!numbers.get(index).equals(number)) {
+    ball++;
+   }
+  }
+ }
+
+ private String mergeResult() {
+  StringBuilder stringBuilder = new StringBuilder(100);
+  if (strike > 0) {
+   stringBuilder.append(strike).append(STRIKE);
+  }
+  if (ball > 0) {
+   stringBuilder.append(ball).append(BALL);
+  }
+  if (strike == 0 && ball == 0) {
+   stringBuilder.append(NOTHING);
+  }
+  return stringBuilder.toString();
+ }
+
+ public boolean isCompleteGame() {
+  return strike == NUMBER_SIZE;
+ }
+
+ private void addNumbers(String userInput) {
+  for (String s : userInput.split("")) {
+   numbers.add(Integer.parseInt(s));
+  }
  }
 
  private String CheckValidationInputData(String userInput) {
